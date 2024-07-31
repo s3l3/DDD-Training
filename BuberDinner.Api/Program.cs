0584139d2041aed2e54636b3b978345287edbf0c
Nikola Seletkovic <165269753+nikolaseletkovic@users.noreply.github.com>
2024-07-31 13:48:36 +0200

    Part 1 - Project Setup and Authentication

---
+++ BuberDinner.Api/Program.cs
@@ -0,0 +1,18 @@
+
+using BuberDinner.Application;
+using BuberDinner.Infrastructure;
+
+var builder = WebApplication.CreateBuilder(args);
+{    
+    builder.Services.AddApplication();
+    builder.Services.AddInfrastructure();
+    builder.Services.AddControllers();
+}
+
+var app = builder.Build();
+{
+    app.UseHttpsRedirection();
+    app.MapControllers();
+    app.Run();
+}
+
---
+++ Docs/Api.md
@@ -0,0 +1,11 @@
+### Login Response
+
+```json
+{
+  "id": "d89c2d91-eb3e-4075-95ff-b920b55aa104",
+  "firstName": "Amichai",
+  "lastName": "Maintinband",
+  "email": "amichai@mantinband.com",
+  "token": "jyjdjB...NFsj"
+}
+```
